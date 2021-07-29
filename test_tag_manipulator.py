02d8b1d2dde37e04c490ceb98b08036fe3eddcb8
unknown <williamsewellart@gmail.com>
2021-07-29 16:09:04 +0100

    cut 2

--- test_tag_manipulator.py
+++ test_tag_manipulator.py
@@ -12,3 +12,16 @@ def test_split_empty_string_result_empty_array():
 
     # assert
     assert result == expResult
+
+def test_split_comma_empty_string_result_empty_array():
+    # arrange
+    stringToSplit = ","
+    expResult = []
+    result = None
+    cut = TagManipulator()
+
+    # act
+    result = cut.parse_string(stringToSplit)
+
+    # assert
+    assert result == expResult
